8450e1528819c83a323af7e01c2ab8cdd79ed83b
fesmpy Dev <fabio_sosapy@outlook.com>
2024-01-11 12:39:58 -0300

    fix all

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-//components
 import Home from "./components/home/Home";
 
-//react router dom
-import { HashRouter, Router, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <Router>
-        <Route path='/' element={<Home />} />
-      </Router>
+      <Routes>
+        <Route path='#/' element={<Home />} />
+      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
