277ae308979d40525413b295988111c2aea8ef18
Fabio Sosa <35641714+fesmpy@users.noreply.github.com>
2024-01-11 12:27:36 -0300

    Update main.jsx
    Fix type Router to Routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import "./index.css";
 import Home from "./components/home/Home";
 
 //react router dom
-import { HashRouter, Router, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <Router>
+      <Routes>
         <Route path='/' element={<Home />} />
-      </Router>
+      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
